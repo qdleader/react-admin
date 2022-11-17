3c6326c5b6220d99ec8150f5857260122a9a30d9
yangkun <yangkun@icourt.cc>
2022-11-17 15:49:08 +0800

    feat: dev与线上环境的区分

--- start.md
+++ start.md
@@ -1,7 +1,16 @@
-
+# 1
 ## 初始化项目 
 
 ```javascript
 pnpm create vite my-vue-app --template vue
 
 ```
+
+# 2.配置开发于生产环境
+
+在vite.config.ts 中
+
+```javascript
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from "vite";
+```
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig, loadEnv, ConfigEnv, UserConfig } from "vite"
+import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()]
+
+// * Vite
+declare type Recordable<T = any> = Record<string, T>
+
+declare interface ViteEnv {
+	VITE_API_URL: string
+	VITE_PORT: number
+	VITE_OPEN: boolean
+	VITE_GLOB_APP_TITLE: string
+	VITE_DROP_CONSOLE: boolean
+	VITE_PROXY_URL: string
+	VITE_BUILD_GZIP: boolean
+	VITE_REPORT: boolean
+}
+
+// Read all environment variable configuration files to process.env
+export function wrapperEnv(envConf: Recordable): ViteEnv {
+	const ret: any = {}
+
+	for (const envName of Object.keys(envConf)) {
+		let realName = envConf[envName].replace(/\\n/g, "\n")
+		realName = realName === "true" ? true : realName === "false" ? false : realName
+
+		if (envName === "VITE_PORT") {
+			realName = Number(realName)
+		}
+		if (envName === "VITE_PROXY") {
+			try {
+				realName = JSON.parse(realName)
+			} catch (error) {
+				console.log(error)
+			}
+		}
+		ret[envName] = realName
+		process.env[envName] = realName
+	}
+	return ret
+}
+
+export default defineConfig((mode: ConfigEnv): UserConfig => {
+	const env = loadEnv(mode.mode, process.cwd())
+	const viteEnv = wrapperEnv(env)
+	return {
+		plugins: [react()],
+	}
 })
